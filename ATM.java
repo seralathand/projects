bb3408bb655efdc7efd86ce4184613a745ded3e7
seralathand <112947951+seralathand@users.noreply.github.com>
2022-09-25 12:46:54 +0530

    Add files via upload

---
+++ ATM.java
@@ -0,0 +1,11 @@
+package MyProject;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+public class ATM {
+    public static void main(String[] args) throws Exception {
+        OptionMenu op=new OptionMenu();
+        op.getLogin();
+    }
+}
